f3ed02a783b159adfa9a192a684815f8459f7a5a
Mickaël FRANCOIS <mickael.francois@fpc-france.com>
2024-08-09 10:16:52 +0200

    Add measureTime demo

---
+++ TechTalks24.MeasureTime/Program.cs
@@ -0,0 +1,35 @@
+﻿using MethodTimer;
+using System.Reflection;
+
+
+namespace TechTalks24.MeasureTime;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var order = new Order();
+        order.CalculatePrice(10);
+    }
+}
+
+
+public class Order
+{
+    [Time]
+    public decimal CalculatePrice(int quantity)
+    {
+        Task.Delay(1000).Wait();
+
+        return quantity * 10;
+    }
+}
+
+
+public static class MethodTimeLogger
+{
+    public static void Log(MethodBase methodBase, TimeSpan timeSpan, string message)
+    {
+        Console.WriteLine($"{methodBase.DeclaringType!.Name}.{methodBase.Name}: {timeSpan}");
+    }
+}
